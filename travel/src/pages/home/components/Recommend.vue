f5659b43e9be4c224a9987e1cf2548658aea26f8
Xanadu-zs <zs_xanadu@163.com>
2019-06-20 14:50:24 +0800

    推荐页面完成

--- travel/src/pages/home/components/Recommend.vue
+++ travel/src/pages/home/components/Recommend.vue
@@ -2,20 +2,14 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <router-link
-        tag="li"
-        class="item border-bottom"
-        v-for="item of list"
-        :key="item.id"
-        :to="'/detail/' + item.id"
-      >
-        <img class="item-img" :src="item.imgUrl" />
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl"/>
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </router-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -23,44 +17,89 @@
 <script>
 export default {
   name: 'HomeRecommend',
-  props: {
-    list: Array
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1905/32/32ad7003179269d2a3.img.jpg_200x200_aee761e4.jpg',
+          title: '谭鸭血老火锅（太古里直营店）',
+          desc: '好吃的板丶真滴偶偶偶偶'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+          title: '成都海昌极地海洋公园',
+          desc: '好玩的板丶真滴偶偶偶偶'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1704/a5/a59677101e07d38a3.img.jpg_200x200_6064e939.jpg',
+          title: '那是飞一样的感觉，国色天乡',
+          desc: '嗨皮的板丶真滴偶偶偶偶'
+        },
+        {
+          id: '0004',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/201407/03/a584374ee80b738e2600d6bcaf1299e3.jpg_200x200_c26fb243.jpg',
+          title: '都江堰',
+          desc: '良辰美景，值得一来'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/mixins.styl'
-  .title
-    margin-top: .2rem
-    line-height: .8rem
-    background: #eee
-    text-indent: .2rem
-  .item
-    overflow: hidden
-    display: flex
-    height: 1.9rem
-    .item-img
-      width: 1.7rem
-      height: 1.7rem
-      padding: .1rem
-    .item-info
-      flex: 1
-      padding: .1rem
-      min-width: 0
-      .item-title
-        line-height: .54rem
-        font-size: .32rem
-        ellipsis()
-      .item-desc
-        line-height: .4rem
-        color: #ccc
-        ellipsis()
-      .item-button
-        line-height: .44rem
-        margin-top: .16rem
-        background: #ff9300
-        padding: 0 .2rem
-        border-radius: .06rem
-        color: #fff
+@import '~styles/mixins.styl';
+
+.title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+
+.item {
+  overflow: hidden;
+  display: flex;
+  height: 1.9rem;
+}
+
+.item-img {
+  width: 1.7rem;
+  height: 1.7rem;
+  padding: 0.1rem;
+}
+
+.item-info {
+  flex: 1;
+  padding: 0.1rem;
+  min-width: 0;
+}
+
+.item-title {
+  line-height: 0.54rem;
+  font-size: 0.32rem;
+  ellipsis();
+}
+
+.item-desc {
+  line-height: 0.4rem;
+  color: #ccc;
+  ellipsis();
+}
+
+.item-button {
+  line-height: 0.44rem;
+  margin-top: 0.16rem;
+  background: #ff9300;
+  padding: 0 0.2rem;
+  border-radius: 0.06rem;
+  color: #fff;
+}
 </style>
--- travel/src/pages/home/components/Weekend.vue
+++ travel/src/pages/home/components/Weekend.vue
@@ -2,13 +2,9 @@
   <div>
     <div class="title">周末去哪儿</div>
     <ul>
-      <li
-        class="item border-bottom"
-        v-for="item of list"
-        :key="item.id"
-      >
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
         <div class="item-img-wrapper">
-          <img class="item-img" :src="item.imgUrl" />
+          <img class="item-img" :src="item.imgUrl">
         </div>
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
@@ -22,32 +18,75 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  props: {
-    list: Array
+  data () {
+    return {
+      weekendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p64/201211/04/9173ff9f33e97f3193835fbb.jpg_640x276_0a0474d5.jpg',
+          title: '成都杜甫草堂',
+          desc: '草堂有水，有桥，有庭院'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1505/b0/b0132e78efa00572.water.jpg_640x276_b87d3bb4.jpg',
+          title: '青城山',
+          desc: '青城山有白素贞？？'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/201309/24/977fea7b3c5cd758c8d65eac.jpg_640x276_915af8e3.jpg',
+          title: '成都大雄猫繁育研究基地',
+          desc: '在这里与各种大小熊猫亲密接触&拍照合影'
+        },
+        {
+          id: '0004',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/201407/03/a584374ee80b738e2600d6bcaf1299e3.jpg_640x276_ba327700.jpg',
+          title: '都江堰',
+          desc: '世界水利工程的鼻祖'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/mixins.styl'
-  .title
-    line-height: .8rem
-    background: #eee
-    text-indent: .2rem
-  .item-img-wrapper
-    overflow: hidden
-    height: 0
-    padding-bottom: 37.09%
-    .item-img
-      width: 100%
-  .item-info
-    padding: .1rem
-    .item-title
-      line-height: .54rem
-      font-size: .32rem
-      ellipsis()
-    .item-desc
-      line-height: .4rem
-      color: #ccc
-      ellipsis()
+@import '~styles/mixins.styl';
+
+.title {
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+
+.item-img-wrapper {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 37.09%;
+}
+
+.item-img {
+  width: 100%;
+}
+
+.item-info {
+  padding: 0.1rem;
+}
+
+.item-title {
+  line-height: 0.54rem;
+  font-size: 0.32rem;
+  ellipsis();
+}
+
+.item-desc {
+  line-height: 0.4rem;
+  color: #ccc;
+  ellipsis();
+}
 </style>
